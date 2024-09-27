7e3ad0cc8c10e94f0911d525487f522fe262dd74
Heverton <hb.hevertonborges@gmail.com>
2024-09-27 15:31:42 -0300

    randomized hyprpaper swither added

---
+++ scripts/change_wallpapers.sh
@@ -0,0 +1,16 @@
+WALLPAPERS_FOLDER=~/Pictures/Wallpapers
+HYPRPAPER_CONFIG=~/.config/hypr/hyprpaper.conf
+
+wallpapers=$(ls -1 $WALLPAPERS_FOLDER | tr '\n' ',')
+
+IFS=',' read -r -a wallpapers_array <<<"$wallpapers"
+
+length_wallpaper_array="${#wallpapers_array}"
+
+ramdom_wallpaper=$(($RANDOM % length_wallpaper_array + 1))
+
+printf "preload = %s%s\n" "$WALLPAPERS_FOLDER/" "${wallpapers_array[ramdom_wallpaper]}" >$HYPRPAPER_CONFIG
+printf "wallpaper = , %s%s\n" "$WALLPAPERS_FOLDER/" "${wallpapers_array[ramdom_wallpaper]}" >>$HYPRPAPER_CONFIG
+
+killall hyprpaper
+hyprpaper &
